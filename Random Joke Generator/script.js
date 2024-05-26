80025322fcd9ce6760312e8fbe31f7a855fc2b01
gouravsemwal321 <113300884+gouravsemwal321@users.noreply.github.com>
2024-05-26 22:07:36 +0530

    Add files via upload

---
+++ Random Joke Generator/script.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById('btn')
+const jokeContainer = document.getElementById('joke')
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+
+let getJoke  = () => {
+    jokeContainer.classList.remove("fade");
+    fetch(url)
+    .then(data => data.json())
+    .then(item => {
+        jokeContainer.textContent = `${item.joke}`;
+        jokeContainer.classList.add("fade");
+    });
+}
+
+btn.addEventListener("click",getJoke)
